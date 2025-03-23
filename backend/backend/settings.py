7a8ad03c5c93034d51d28f8857bcdb340118b8f7
Your Name <you@example.com>
2025-03-23 21:17:08 +1000

    Your commit message

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'backend'
+    'backend',
+    'teachers'
 ]
 
 MIDDLEWARE = [
@@ -78,11 +79,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+	'default': {
+    #'ENGINE': 'django.db.backends.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'data_base_name',
+    'USER': 'username',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'PORT': '5432',
     }
 }
+
 
 
 # Password validation
