236fa9fb3fdf980ed7b850049c635513d116ea74
Kailash Kidd <kiddkailash@gmail.com>
2025-03-25 00:17:49 +1000

    Init Dummy Files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,22 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
-// Webpages
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import Register from "./pages/Register";
+// Webpages - Public
+import Login from "./pages/public/Login";
+import PageNotFound from "./pages/public/PageNotFound";
+import Register from "./pages/public/Register";
+// Webpages - Private
+import Account from "./pages/private/Account";
+import Activity from "./pages/private/Activity";
+import AIAssistant from "./pages/private/AIAssistant";
+import Analytics from "./pages/private/Analytics";
+import Attendance from "./pages/private/Attendance";
+import Calendar from "./pages/private/Calendar";
+import Message from "./pages/private/Message";
+import Storage from "./pages/private/Storage";
+import Students from "./pages/private/Students";
+import Dashboard from "./pages/private/Dashboard";
+import Settings from "./pages/private/Settings";
 
 // Components
 import ProtectRoute from "./components/ProtectRoutes";
@@ -32,6 +44,17 @@ function App() {
     { path: "/", component: <Login /> },
     { path: "/login", component: <Login /> },
     { path: "/register", component: <Register /> },
+    { path: "/dashboard", component: <Dashboard /> },
+    { path: "/account", component: <Account /> },
+    { path: "/activity", component: <Activity /> },
+    { path: "/ai-assistant", component: <AIAssistant /> },
+    { path: "/analytics", component: <Analytics /> },
+    { path: "/attendance", component: <Attendance /> },
+    { path: "/calendar", component: <Calendar /> },
+    { path: "/message", component: <Message /> },
+    { path: "/storage", component: <Storage /> },
+    { path: "/students", component: <Students /> },
+    { path: "/settings", component: <Settings /> },
 
     // Add additional page objects here as needed.
   ];
@@ -81,7 +104,8 @@ function App() {
                   page.component
                 ) : (
                   // Render protected component wrapped with ProtectRoute.
-                  <ProtectRoute>{page.component}</ProtectRoute>
+                  // <ProtectRoute>{page.component}</ProtectRoute>
+                  page.component
                 )
               }
             />
