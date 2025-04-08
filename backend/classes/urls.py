bf872fe6a0e7f73c96bfdf396fc5d1604f92b51e
shivamtrivedi <shivammtrivedi@gmail.com>
2025-04-08 11:50:12 +1000

    changed classes urls

--- backend/classes/urls.py
+++ backend/classes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import create_class, get_all_classes, upload_students_csv
 
 urlpatterns = [
-    path('classes/create/', create_class, name='create_class'),
+    path('create/', create_class, name='create_class'),
     path('', get_all_classes, name="get_all_classes"),
-    path("classes/upload-csv/", upload_students_csv, name="upload_students_csv"),
+    path("upload-csv/", upload_students_csv, name="upload_students_csv"),
 ]
