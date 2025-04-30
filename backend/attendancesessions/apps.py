5f6095ab463d3e8b1ed7706db9b560b1102efbd4
Kailash Kidd <kiddkailash@gmail.com>
2025-04-30 22:42:34 +1000

    Cascade Delete Data

--- backend/attendancesessions/apps.py
+++ backend/attendancesessions/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AttendancesessionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'attendancesessions'
+
+    def ready(self):
+        import attendancesessions.signals
