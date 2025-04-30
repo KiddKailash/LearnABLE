c93c5124346ca1351e76d78179f79eed49018f35
shivamtrivedi <shivammtrivedi@gmail.com>
2025-04-30 12:18:36 +1000

    added back learning material view set

--- backend/learningmaterial/views.py
+++ backend/learningmaterial/views.py
@@ -6,6 +6,27 @@ from .serializers import LearningMaterialsSerializer
 from django.conf import settings
 import os
 
+class LearningMaterialsViewSet(viewsets.ModelViewSet):
+    queryset = LearningMaterials.objects.all()
+    serializer_class = LearningMaterialsSerializer
+    parser_classes = (MultiPartParser, FormParser)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+
+        if hasattr(request.user, 'teacher'):
+            data['created_by'] = request.user.teacher.id
+        else:
+            return Response({"error": "User is not linked to a teacher."}, status=400)
+
+        serializer = self.get_serializer(data=data)
+        if serializer.is_valid():
+            instance = serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
+        
+
 class LearningMaterialUploadView(APIView):
     def post(self, request):
         title = request.data.get('title')
