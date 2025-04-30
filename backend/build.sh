b1879070c4d7ca1f1d6033c82a2b9d2c7aa7fc3a
Kailash Kidd <kiddkailash@gmail.com>
2025-04-30 11:00:43 +1000

    Updated Build File

--- backend/build.sh
+++ backend/build.sh
@@ -5,11 +5,9 @@ set -o errexit
 # Install dependencies using Poetry
 poetry install --no-root
 
-# Activate the virtual environment
-python -m poetry env info -p > /dev/null && source "$(poetry env info -p)/bin/activate" || true
-
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Apply database migrations
+# Apply database migrations (be more explicit)
+python manage.py makemigrations teachers students subjects classes attendancesessions studentattendance classstudents assessments studentgrades learningmaterial nccdreports
 python manage.py migrate
